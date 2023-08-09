be60ac0971ee14c44f8f495d8a4c18afbf2c066b
Md. Arifin Ahmed Rafi <ahmedrafi364@gmail.com>
2023-08-09 21:44:42 +0600

    animation added

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import banner1 from '../images/roboway.jpg'
+import banner1 from '../images/cover1.jpg'
+import banner2 from '../images/cover2.jpg'
+import banner3 from '../images/cover3.jpg'
 import './Carousel.css'
 import Navbar from '../pages/Navbar';
 
@@ -16,7 +18,7 @@ const Carousel = () => {
     </div>
   </div> 
   <div id="slide2" className="carousel-item relative w-full">
-    <img src={banner1} className="w-full" />
+    <img src={banner2} className="w-full" />
     <div className="absolute flex justify-between transform -translate-y-1/2 right-5 bottom-1">
       <a href="#slide1" className="mx-10 btn btn-circle">❮</a> 
       <a href="#slide3" className="btn btn-circle">❯</a>
--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
+import {FaBeer} from 'react-icons/fa';
 
 const Navbar = () => {
     
     return (
         
-            <div className="navbar  text-white bg-gradient-to-r fixed grid grid-cols-3  from-purple-800/[0.8] to-red-600/[0.8] shadow-2xl ">
+            <div className=" bg-opacity-40 top-0 z-50 backdrop-blur-lg rounded shadow-2xl w-full   navbar  text-white bg-gradient-to-r fixed grid grid-cols-3  from-purple-800/[0.8] to-red-600/[0.8] shadow-2xl ">
   <div>
   <Link to='/' className="btn btn-ghost normal-case text-xl">VMDB</Link>
   <Link to='/Home' className="btn btn-ghost normal-case">Home</Link>
   <Link to='/Login' className="btn btn-ghost normal-case">Login</Link>
+  <Link to='/Profile' className="btn btn-ghost normal-case">Search</Link>
+  
   </div>
   <form action="" className=''>
   <input type="text" placeholder="Search your mentor " className="input rounded-2xl input-bordered text-black w-full items-center"/>
